04c545cd987e8490c547ad14ff18c72e5d9280c3
Prashant Moghariya <prashant.moghariya@kgn-software.com>
2024-02-26 19:18:19 +0530

    requested booking work in progress

---
+++ src/components/Design/YourBooking/bookingSidebar.js
@@ -0,0 +1,26 @@
+"use client";
+import React, { useRef, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./bookingSidebar.module.scss";
+
+function BookingSidebar() {
+  
+  return (
+    <div className={styles.bookingSidebarPage}>
+      <ul>
+        <li>
+            <Link className="activeSidebar" href="">Requested Bookings</Link>
+        </li>
+        <li>
+            <Link href="">Confirmed Bookings</Link>
+        </li>
+        <li>
+            <Link href="">Booking History</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookingSidebar;
