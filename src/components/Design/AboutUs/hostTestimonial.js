38a73b4d50603860868c71b6df24c504cb2c0aa5
Prashant Moghariya <prashant.moghariya@kgn-software.com>
2024-03-12 12:25:22 +0530

    responsive work

--- src/components/Design/AboutUs/hostTestimonial.js
+++ src/components/Design/AboutUs/hostTestimonial.js
@@ -18,7 +18,7 @@ function HostTestimonials() {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 1,
             },
           },
           {
